e70e521eb6a02457b82fa3818a90e944b943d614
Anh <blackoxy@gmail.com>
2018-09-11 09:30:03 -0600

    Basic route

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`listening on ${port}`)
+    console.log(`listening on ${port}`)    
+})
+
+app.get('/', (req, res)=> {
+    res.send('THE ROUTE WORKED!')
 })
